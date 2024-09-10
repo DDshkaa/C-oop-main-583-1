d46335bbe450fbd3b5dbd7255918bdea38d0641a
DDshkaa <1dragin@bk.ru>
2024-09-10 19:51:08 +0700

    Добавьте файлы проекта.

---
+++ PRACTIC NOOB C#/PRACTIC NOOB C#/Program.cs
@@ -0,0 +1,28 @@
+﻿void SayHelloRu()
+{
+    Console.WriteLine("Привет");
+}
+void SayHelloEn()
+{
+    Console.WriteLine("Hello");
+}
+void SayHelloFr()
+{
+    Console.WriteLine("Salut");
+}
+
+
+string language = "en";
+
+switch (language)
+{
+    case "en":
+        SayHelloEn();
+        break;
+    case "ru":
+        SayHelloRu();
+        break;
+    case "fr":
+        SayHelloFr();
+        break;
+}
