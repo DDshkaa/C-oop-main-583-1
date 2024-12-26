630525a01bb6f01d7f83cda269b61cdc5b9f55d6
DDshkaa <1dragin@bk.ru>
2024-12-26 19:06:49 +0700

    Add files via upload

---
+++ Проект12/Проект12/ListItem.h
@@ -0,0 +1,13 @@
+//TODO: rename+
+//! \brief Структура для узла двусвязного списка
+struct ListItem
+{
+	//! \brief Данные узла
+	int Data;
+
+	//! \brief Указатель на следующий узел
+	ListItem* Next;
+
+	//! \brief Указатель на предыдущий узел     
+	ListItem* Prev;
+};
